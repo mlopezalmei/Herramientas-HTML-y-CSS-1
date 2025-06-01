9ed21da9be947728aaeecb35cd7037d10ede6ac4
Manuel López Almeida <mlopezalmei@uoc.edu>
2025-06-01 17:04:45 +0200

    recorte libreria Font Awesome

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { library, dom } from '@fortawesome/fontawesome-svg-core';
+import { faHome, faBook, faLink } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faHome, faBook, faLink);
+dom.watch();
